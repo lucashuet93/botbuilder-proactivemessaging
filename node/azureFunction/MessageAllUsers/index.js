2ed03e1136be7feca71c3a149de254cd4bd7a962
lucashuet93 <lucashh@microsoft.com>
2019-02-26 15:43:55 +0100

    finish messageAllUsers - bind to cosmos db, post message to all users

--- node/azureFunction/MessageAllUsers/index.js
+++ node/azureFunction/MessageAllUsers/index.js
@@ -1,16 +1,50 @@
-module.exports = async function (context, req) {
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+    // pull all cosmos entries, each of which is a conversation reference
+    let conversationReferences = context.bindings.allDocuments;
+
+    // loop through cosmos entries
+    let count = 0;
+    for (let conversationReference of conversationReferences) {
+        count++;
+        // isolate conversation reference from entry
+        const reference = {
+            activityId: conversationReference.activityId,
+            user: conversationReference.user,
+            bot: conversationReference.bot,
+            conversation: conversationReference.conversation,
+            channelId: conversationReference.channelId,
+            serviceUrl: conversationReference.serviceUrl
+        }
+        context.log(reference);
+        // hit proactive endpoint with message and conversation reference
+        const postBody = {
+            reference: reference,
+            message: req.body.message
         };
+        fetch(process.env.ProactiveEndpoint, {
+            method: 'POST',
+            body: JSON.stringify(postBody),
+            headers: { 'Content-Type': 'application/json' }
+        }).then((res) => {
+            if (count === conversationReferences.length) {
+                context.res = {
+                    status: 200
+                };
+                context.done();
+            };
+        });
     }
-    else {
+    
+    // handle edge case
+    if(conversationReferences.length === 0){
         context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            status: 200
         };
+        context.done();
     }
 };
