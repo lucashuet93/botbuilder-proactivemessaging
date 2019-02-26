21d22c84e01579d908c5986ef585be5542ccca41
lucashuet93 <lucashh@microsoft.com>
2019-02-26 16:14:11 +0100

    assume user may have entries on multiple channels, notify all

--- node/azureFunction/MessageSpecificUser/index.js
+++ node/azureFunction/MessageSpecificUser/index.js
@@ -5,12 +5,13 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     // pull cosmos entry for specific user
-    let conversationReferences = context.bindings.userSpecificDocument;
-
-    // cosmos entry for given user is first value in conversationReferences array
-    if (conversationReferences.length > 0) {
+    let conversationReferences = context.bindings.userSpecificDocuments;
+    
+    // loop through cosmos entries
+    let count = 0;
+    for (let conversationReference of conversationReferences) {
+        count++;
         // isolate conversation reference from entry
-        let conversationReference = conversationReferences[0];
         const reference = {
             activityId: conversationReference.activityId,
             user: conversationReference.user,
@@ -19,7 +20,6 @@ module.exports = function (context, req) {
             channelId: conversationReference.channelId,
             serviceUrl: conversationReference.serviceUrl
         }
-
         // hit proactive endpoint with message and conversation reference
         const postBody = {
             reference: reference,
@@ -30,16 +30,21 @@ module.exports = function (context, req) {
             body: JSON.stringify(postBody),
             headers: { 'Content-Type': 'application/json' }
         }).then((res) => {
-            context.res = {
-                status: 200,
-                body: "User has been notified"
+            if (count === conversationReferences.length) {
+                context.res = {
+                    status: 200,
+                    body: "User has been notified on all channels"
+                };
+                context.done();
             };
-            context.done();
         });
-    } else {
+    }
+    
+    // handle edge case
+    if(conversationReferences.length === 0){
         context.res = {
             status: 200,
-            body: "No entry found"
+            body: "No entries found"
         };
         context.done();
     }
