6c27c2b9ff10cb032684c384e671c0c2cc179921
Konstantin Kichinsky <constantin@kichinsky.ru>
2019-02-28 14:19:38 +0100

    Added optional endpoint for the broadcast method

--- node/broadcastingSample/bot/services/broadcastService.js
+++ node/broadcastingSample/bot/services/broadcastService.js
@@ -5,9 +5,13 @@ class BroadcastService {
         this.botBroadcastEndpoint = botBroadcastEndpoint;
     }
 
-    async broadcast(broadCastList) {
+    async broadcast(broadCastList, broadcastEndpoint) {
+        if (broadcastEndpoint === undefined) {
+            broadcastEndpoint = this.botBroadcastEndpoint;
+        }
+
         // send messages to all the referenced conversations
-        await fetch(this.botBroadcastEndpoint, {
+        await fetch(broadcastEndpoint, {
             method: 'POST',
             body: JSON.stringify(broadCastList),
             headers: { 'Content-Type': 'application/json' }
