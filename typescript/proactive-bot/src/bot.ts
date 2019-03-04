71151bd3a57195b87529f8fcff621a086a9cb037
Konstantin Kichinsky <constantin@kichinsky.ru>
2019-03-04 13:00:33 +0300

    Refactored code to send messages in separate functions

--- typescript/proactive-bot/src/bot.ts
+++ typescript/proactive-bot/src/bot.ts
@@ -9,13 +9,27 @@ export class ProactiveBot {
      *
      * @param {TurnContext} context on turn context object.
      */
-    public onTurn = async (turnContext: TurnContext) => {
+    public onTurn = async (context: TurnContext) => {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
-        if (turnContext.activity.type === ActivityTypes.Message) {
-            await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
-        } else {
-            // Generic handler for all other activity types.
-            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
+        if (context.activity.type === ActivityTypes.Message) {
+            await this.sendEchoMessage(context, context.activity.text);
+        } else if (context.activity.type === ActivityTypes.ConversationUpdate) {
+            // When the user (not the bot) enters the conversation, greet them
+            const memberAdded = context.activity.membersAdded.length !== 0;
+            const notBot = context.activity.membersAdded[0].id !== context.activity.recipient.id;
+            if (memberAdded && notBot) {
+                await this.sendWelcomeMessage(context);
+            }
         }
+    }
+
+    private async sendWelcomeMessage(context: TurnContext) {
+        const welcomeMessage = `**Echo**: *Hello, friend*!`;
+        await context.sendActivity(welcomeMessage);
+    }
+
+    private async sendEchoMessage(context: TurnContext, msg: string) {
+        const echoMessage = `**Echo**: *${msg}*`;
+        await context.sendActivity(echoMessage);
     }
 }
