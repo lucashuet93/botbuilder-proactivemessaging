309ff8fa308f42b29dce4d5779a6b8deb0c2932f
Katie Prochilo <43346846+KatieProchilo@users.noreply.github.com>
2019-04-11 15:44:47 -0700

    Removed all refs to .bot file

--- node/advanced-sample/bot/index.js
+++ node/advanced-sample/bot/index.js
@@ -11,24 +11,12 @@ const CosmosClient = require('@azure/cosmos').CosmosClient;
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const { BotFrameworkAdapter } = require('botbuilder');
 
-// Import required bot configuration.
-const { BotConfiguration } = require('botframework-config');
-
 // This bot's main dialog.
 const { MyBot } = require('./bot');
 
-// Read botFilePath and botFileSecret from .env file
-// Note: Ensure you have a .env file and include botFilePath and botFileSecret.
+// Note: Ensure you have a .env file.
 const ENV_FILE = path.join(__dirname, '.env');
 dotenv.config({ path: ENV_FILE });
-
-// bot endpoint name as defined in .bot file
-// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
-const DEV_ENVIRONMENT = 'development';
-
-// bot name as defined in .bot file
-// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
-const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
 
 // Create HTTP server
 const server = restify.createServer();
@@ -40,29 +28,11 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
 // add body parser
 server.use(restify.plugins.bodyParser());
 
-// .bot file path
-const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
-
-// Read bot configuration from .bot file.
-let botConfig;
-try {
-  botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
-} catch (err) {
-  console.error(`\nError reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
-  console.error(`\n - The botFileSecret is available under appsettings for your Azure Bot Service bot.`);
-  console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.`);
-  console.error(`\n - See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.\n\n`);
-  process.exit();
-}
-
-// Get bot endpoint configuration by service name
-const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
-
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration.
 const adapter = new BotFrameworkAdapter({
-  appId: endpointConfig.appId || process.env.microsoftAppID,
-  appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
+  appId: process.env.microsoftAppID || "",
+  appPassword: process.env.microsoftAppPassword || ""
 });
 
 // Catch-all for errors.
