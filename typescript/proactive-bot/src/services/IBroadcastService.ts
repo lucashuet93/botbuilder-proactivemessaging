0b7c0a07d3a99d77baca8b6463cd4af0cb628a74
Ryan <ryanvolum@users.noreply.github.com>
2019-04-15 13:35:04 -0700

    Revert "TypeScript implementation for proactive bot"

--- typescript/proactive-bot/src/services/IBroadcastService.ts
+++
@@ -1,5 +0,0 @@
-import { ConversationReference } from "botbuilder";
-
-export interface IBroadcastService {
-    broadcast(references: Array< Partial<ConversationReference> >, message: string);
-}
--- typescript/proactive-bot/src/services/IBroadcastStorageService.ts
+++
@@ -1,6 +0,0 @@
-import { ConversationReference, TurnContext } from "botbuilder";
-import { IConversationStorageService } from "./IConversationStorageService";
-
-export interface IBroadcastStorageService extends IConversationStorageService {
-    restoreAllReferences(context: TurnContext): Promise< Array< Partial< ConversationReference > > >;
-}
